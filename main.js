45a08c56d93bfee03cb09ddc9abbec586b03c090
Anita-Rosikhina <anita.rosikhina@hebronsoft.com>
2022-03-10 16:42:50 +0200

    - implemented update select options

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let list = document.querySelector('.list')
 let btnSubmit = document.querySelector('.btn_submit')
+let select = document.querySelector('select')
 let serialNumber = document.form.number
 let typeFigure = document.form.type
 let rotation = document.form.rotation
@@ -8,10 +9,12 @@ let date = new Date()
 let hours = date.getHours()
 let minutes = date.getMinutes()
 
+// submit
 btnSubmit.addEventListener('click', () => {
     createElement(serialNumber.value,typeFigure.value, rotation.value, color.value)
     reset()
     resort()
+    updateSelectOptions()
 })
 
 function createElement (serialNumber, typeFigure, rotation, color) {
@@ -79,6 +82,7 @@ function removeElement() {
     document.querySelectorAll('.block').forEach((el, i) => {
         document.querySelectorAll('.btn_delete')[i].addEventListener('click', () => {
             document.querySelector(`.block[data-number="${el.attributes['data-number'].value}"]`).remove()
+            updateSelectOptions()
         })
     })
 }
@@ -91,6 +95,18 @@ function resort() {
         return 0;
     })
     sorted.forEach(e => list.appendChild(e));
+}
+
+function updateSelectOptions() {
+    const arr = Array.from(document.querySelectorAll('.block'))
+    const existingSerialNumbers = arr.map(el => +el.attributes['data-number'].value)
+    const availableSerialNumbers =
+        Array.from({length: 10}, (_, i) => i + 1)
+            .filter(number => !existingSerialNumbers.includes(number))
+    select.innerHTML = ''
+    availableSerialNumbers.forEach(el => {
+        select.innerHTML += `<option value="${el}">${el}</option>`
+    })
 }
 
 function reset() {
